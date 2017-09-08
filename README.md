367b6a2a9f38a2d5b96277c6d0e00b373ea6eeed
Björn Ritzl <bjorn.ritzl@king.com>
2017-09-08 09:09:20 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# gooey
-Defold GUI system
+# Gooey
+Gooey is a GUI system for the [Defold](https://www.defold.com) game engine. It is inspired by the excellent [Dirty Larry](https://github.com/andsve/dirtylarry) library, but with modifications that make it stand apart.
+
+# Installation
+You can use Gooey in your own project by adding this project as a [Defold library dependency](http://www.defold.com/manuals/libraries/). Open your game.project file and in the dependencies field under project add:
+
+https://github.com/britzl/gooey/archive/master.zip
+
+# Usage
+The Gooey system is encapsulated in a single Lua module without any visual components. It makes very little assumptions of the look and feel of the UI components it supports. Instead Gooey focuses on providing stable input and state handling and lets the user decide which states matter and how they should be presented visually. Gooey supports the following component types:
+
+* Button
+* Checkbox
+* Radio button
+* List
+* Input text
+
+## gooey.button(node_id, action_id, action, fn)
+Perform input and state handling for a button
+
+**PARAMETERS**
+* ```node_id``` (string|hash) - Id of the node representing the clickable area of the button, typically the background of the button
+* ```action_id``` (hash) - Action id as received from on_input()
+* ```action``` (table) - Action as received from on_input()
+* ```fn``` (function) - Function to call when the button is clicked/tapped on. A button is considered clicked/tapped if both a pressed and released action has been detected inside the bounds of the node. The function will get the same button state table as described below passed as its first argument
+
+**RETURN**
+* ```button``` (table) - State data for the button based on current and previous input actions
+
+The button table contains the following fields:
+
+* ```node``` (userdata) - The node itself
+* ```enabled``` (boolean) - true if the node is enabled
+* ```over``` (boolean) - true if user action is inside the node
+* ```over_now``` (boolean) - true if user action moved inside the node this call
+* ```out_now``` (boolean) - true if user action moved outside the node this call
+* ```pressed``` (boolean) - true if the button is pressed
+* ```pressed_now``` (boolean) - true if the button was pressed this call
+* ```released_now``` (boolean) - true if the button was released this call
+
+**EXAMPLE**
+
+	local gooey = require "gooey.gooey"
+
+	local function update_button(button)
+		if button.pressed_now then
+			gui.play_flipbook(button.node, hash("button_pressed"))
+		elseif button.released_now then
+			gui.play_flipbook(button.node, hash("button_normal"))
+		elseif not button.pressed and button.over_now then
+			gui.play_flipbook(button.node, hash("button_over"))
+		elseif not button.pressed and button.out_now then
+			gui.play_flipbook(button.node, hash("button_normal"))
+		end
+	end
+
+	function on_input(self, action_id, action)
+		local button = gooey.button("button/bg", action_id, action, function(button)
+			print("pressed")
+		end)
+	end
+
+## gooey.checkbox(node_id, action_id, action, fn)
+Perform input and state handling for a checkbox
+
+**PARAMETERS**
+* ```node_id``` (string|hash) - Id of the node representing the clickable area of the checkbox
+* ```action_id``` (hash) - Action id as received from on_input()
+* ```action``` (table) - Action as received from on_input()
+* ```fn``` (function) - Function to call when the checkbox is checked/unchecked on. A checkbox is considered checked/unchecked if both a pressed and released action has been detected inside the bounds of the node. The function will get the same checkbox state table as described below passed as its first argument
+
+**RETURN**
+* ```checkbox``` (table) - State data for the checkbox based on current and previous input actions
+
+The checkbox table contains the following fields:
+
+* ```node``` (userdata) - The node itself
+* ```enabled``` (boolean) - true if the node is enabled
+* ```over``` (boolean) - true if user action is inside the node
+* ```over_now``` (boolean) - true if user action moved inside the node this call
+* ```out_now``` (boolean) - true if user action moved outside the node this call
+* ```checked``` (boolean) - The checkbox state (checked/unchecked)
+* ```pressed``` (boolean) - true if the checkbox is pressed (ie mouse/touch down but not yet released)
+* ```pressed_now``` (boolean) - true if the checkbox was pressed this call
+* ```released_now``` (boolean) - true if the checkbox was released this call
+
+**EXAMPLE**
+
+	local gooey = require "gooey.gooey"
+
+	local function update_checkbox(checkbox)
+		if checkbox.released_now then
+			if checkbox.checked then
+				gui.play_flipbook(checkbox.node, hash("checkbox_checked"))
+			else
+				gui.play_flipbook(checkbox.node, hash("checkbox_unchecked"))
+			end
+		elseif not checkbox.pressed and checkbox.over_now then
+			gui.play_flipbook(checkbox.node, hash("checkbox_over"))
+		elseif not checkbox.pressed and checkbox.out_now then
+			gui.play_flipbook(checkbox.node, hash("checkbox_normal"))
+		end
+	end
+
+	function on_input(self, action_id, action)
+		local checkbox = gooey.checkbox("checkbox/bg", action_id, action, function(checkbox)
+			print("checked", checkbox.checked)
+		end)
+	end
---
+++ gooey/gooey.lua
@@ -0,0 +1,317 @@
+local M = {}
+
+local TOUCH = hash("touch")
+local TEXT = hash("text")
+local MARKED_TEXT = hash("marked_text")
+local BACKSPACE = hash("backspace")
+
+local EMPTY = hash("")
+
+local buttons = {}
+local checkboxes = {}
+local radiobuttons = {}
+local lists = {}
+local inputfields = {}
+
+local utf8_gfind = "([%z\1-\127\194-\244][\128-\191]*)"
+
+-- Convert string to hash, unless it's already a hash
+-- @param str String to convert
+-- @return The hashed string
+local function to_hash(str)
+	return type(str) == "string" and hash(str) or id
+end
+
+--- Create a unique key for an hash by combining the id with the current url
+-- @param hsh Hash to create key for
+-- @return Unique key based on the hash and the current url 
+local function to_key(hsh)
+	local url = msg.url()
+	return hash_to_hex(url.socket or EMPTY)
+		.. hash_to_hex(url.path or empty)
+		.. hash_to_hex(url.fragment or empty)
+		.. hash_to_hex(hsh)
+end
+
+--- Get an instance (table) for a node or create one if it doesn't
+-- exist
+-- @param node_id
+-- @param instances
+-- @return Instance for the node
+local function instance(node_id, instances)
+	local key = to_key(node_id)
+	instances[key] = instances[key] or {}
+	return instances[key]
+end	
+
+--- Convenience function to acquire input focus
+function M.acquire_input()
+	msg.post(".", "acquire_input_focus")
+end
+
+--- Convenience function to release input focus
+function M.release_input()
+	msg.post(".", "release_input_focus")
+end
+
+--- Check if a node is enabled. This is done by not only
+-- looking at the state of the node itself but also it's
+-- ancestors all the way up the hierarchy
+-- @param node
+-- @return true if node and all ancestors are enabled
+function M.is_enabled(node)
+	local parent = gui.get_parent(node)
+	if parent then
+		return M.is_enabled(parent)
+	end
+	return gui.is_enabled(node)
+end
+
+--- Mask text by replacing every character with a mask
+-- character
+-- @param text
+-- @param mask
+-- @return Masked text
+function M.mask_text(text, mask)
+	mask = mask or "*"
+	local masked_text = ""
+	for uchar in string.gfind(text, utf8_gfind) do
+		masked_text = masked_text .. mask
+	end
+	return masked_text
+end
+
+
+function M.button(node_id, action_id, action, fn)
+	node_id = to_hash(node_id)
+	local node = gui.get_node(node_id)
+	assert(node)
+
+	local button = instance(node_id, buttons)
+	button.enabled = M.is_enabled(node)
+	button.node = node
+	
+	local over = gui.pick_node(node, action.x, action.y)
+	button.over_now = over and not button.over
+	button.out_now = not over and button.over
+	button.over = over
+
+	if not button.enabled then
+		button.pressed_now = false
+		button.released_now = false
+	else
+		local touch = action_id == TOUCH
+		local pressed = touch and action.pressed and button.over
+		local released = touch and action.released
+		button.pressed_now = pressed and not button.pressed
+		button.released_now = released and button.pressed
+		button.pressed = pressed or (button.pressed and not released)
+		if button.released_now and button.over then
+			fn(button)
+		end
+	end
+	return button
+end
+
+
+function M.checkbox(node_id, action_id, action, fn)
+	node_id = to_hash(node_id)
+	local node = gui.get_node(node_id)
+	assert(node)
+
+	local checkbox = instance(node_id, checkboxes)
+	checkbox.enabled = M.is_enabled(node)
+	checkbox.node = node
+
+	local over = gui.pick_node(node, action.x, action.y)
+	checkbox.over_now = over and not checkbox.over
+	checkbox.out_now = not over and checkbox.over
+	checkbox.over = over
+
+	if not checkbox.enabled then
+		checkbox.pressed_now = false
+		checkbox.released_now = false
+	else
+		local touch = action_id == TOUCH
+		local pressed = touch and action.pressed and checkbox.over
+		local released = touch and action.released
+		checkbox.pressed_now = pressed and not checkbox.pressed
+		checkbox.released_now = released and checkbox.pressed
+		checkbox.pressed = pressed or (checkbox.pressed and not released)
+		if checkbox.released_now and checkbox.over then
+			checkbox.checked = not checkbox.checked
+			fn(checkbox)
+		end
+	end
+	return checkbox
+end
+
+
+function M.radio(node_id, group_id, action_id, action, fn)
+	node_id = to_hash(node_id)
+	group_id = to_hash(group_id)
+	local node = gui.get_node(node_id)
+	assert(node)
+
+	local radio = instance(node_id, radiobuttons)
+	radio.enabled = M.is_enabled(node)
+	radio.node = node
+	radio.group = to_key(group_id)
+
+	local over = gui.pick_node(node, action.x, action.y)
+	radio.over_now = over and not radio.over
+	radio.out_now = not over and radio.over
+	radio.over = over
+
+	if not radio.enabled then
+		radio.pressed_now = false
+		radio.released_now = false
+	else
+		local touch = action_id == TOUCH
+		local pressed = touch and action.pressed and radio.over
+		local released = touch and action.released
+		radio.pressed_now = pressed and not radio.pressed
+		radio.released_now = released and radio.pressed
+		radio.pressed = pressed or (radio.pressed and not released)
+		if radio.released_now and radio.over then
+			radio.selected = true
+			for _,rb in pairs(radiobuttons) do
+				if rb ~= radio and rb.group == radio.group then
+					rb.selected = false
+				end
+			end
+			fn(radio)
+		end
+	end
+	return radio
+end
+
+
+function M.list(root_id, node_ids, action_id, action, fn)
+	root_id = to_hash(root_id)
+	local root = gui.get_node(root_id)
+	assert(root)
+
+	local list = instance(root_id, lists)
+	list.enabled = M.is_enabled(root)
+	list.root = root
+	list.nodes = {}
+
+	if #node_ids == 0 then return list end
+	
+	local over_node
+	local over_index
+	for i=1,#node_ids do
+		local node = gui.get_node(node_ids[i])
+		list.nodes[i] = node
+		if gui.pick_node(node, action.x, action.y) then
+			over_node = node
+			over_index = i
+		end		
+	end
+	list.over = over_node ~= nil
+	list.over_node = over_node
+	list.over_index = over_index
+
+	local first_node = list.nodes[1]
+	local last_node = list.nodes[#list.nodes]
+	local total_height = math.abs(gui.get_position(last_node).y) + gui.get_size(last_node).y / 2
+	local list_height = gui.get_size(list.root).y
+
+	if action_id == TOUCH then
+		if not list.enabled then
+			list.pressed_node = nil
+			list.pressed_index = nil
+			list.selected_node = nil
+			list.selected_index = nil
+		elseif action.pressed and list.over then
+			list.pressed_node = over_node
+			list.pressed_index = over_index
+			list.root_pos = gui.get_position(root)
+			list.action_pos = vmath.vector3(action.x, action.y, 0)
+		elseif action.released then
+			list.pressed_node = nil
+			list.pressed_index = nil
+			if list.over and not list.scrolling then
+				list.selected_node = over_node
+				list.selected_index = over_index
+				fn(list)
+			end
+		end
+	end
+	
+	if list.pressed_node then
+		local amount = vmath.vector3(action.x, action.y, 0) - list.action_pos
+		amount.x = 0
+		list.scrolling = math.abs(amount.y) > 10
+		local root_pos = list.root_pos + amount
+		root_pos.y = math.min(root_pos.y, total_height - list_height)
+		root_pos.y = math.max(root_pos.y, 0)
+		gui.set_position(list.root, root_pos)
+	end
+	
+	return list
+end
+
+-- from dirty larry with modifications
+function M.input(node_id, keyboard_type, action_id, action)
+	node_id = to_hash(node_id)
+	local node = gui.get_node(node_id)
+	assert(node)
+
+	local input = instance(node_id, inputfields)
+	input.enabled = M.is_enabled(node)
+	input.node = node
+	input.over = gui.pick_node(node, action.x, action.y)
+	input.text = input.text or ""
+	input.marked_text = input.marked_text or ""
+	input.keyboard_type = keyboard_type
+
+	if action.pressed then
+		input.pressed = true
+	elseif action.released then
+		if input.over then
+			gui.reset_keyboard()
+			gui.show_keyboard(keyboard_type, true)
+			input.selected = true
+			input.marked_text = ""
+		else
+			if input.pressed then
+				gui.hide_keyboard()
+			end
+			input.selected = false
+		end
+		input.pressed = false
+	end
+	
+	if input.selected then
+		-- new raw text input
+		if action_id == TEXT then
+			input.text = input.text .. action.text
+			input.marked_text = ""
+		-- new marked text input (uncommitted text)
+		elseif action_id == MARKEDTEXT then
+			input.marked_text = action.text or ""
+		-- input deletion
+		elseif action_id == BACKSPACE and (action.pressed or action.repeated) then
+			local last_s = 0
+			for uchar in string.gfind(input.text, utf8_gfind) do
+				last_s = string.len(uchar)
+			end
+			input.text = string.sub(input.text, 1, string.len(input.text) - last_s)
+		end
+		
+		if keyboard_type == gui.KEYBOARD_TYPE_PASSWORD then
+			input.masked_text = M.mask_text(input.text, "*")
+			input.masked_marked_text = M.mask_text(input.marked_text, "*")
+		else
+			input.masked_text = nil
+			input.masked_marked_text = nil
+		end
+	end
+	
+	return input
+end
+
+
+return M
