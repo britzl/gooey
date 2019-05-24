2351f88ed3bd98eecab9f1471d9434809dce4500
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-05-24 06:13:53 +0200

    Update run.sh

--- .travis/run.sh
+++ .travis/run.sh
@@ -33,7 +33,7 @@ if [ -n "${DEFOLD_AUTH}" ] && [ -n "${DEFOLD_USER}" ]; then
 fi
 
 echo "Running bob.jar - building"
-java -jar bob.jar --debug build --keep-unused
+java -jar bob.jar --debug --keep-unused build
 
 echo "Starting dmengine_headless"
 if [ -n "${DEFOLD_BOOSTRAP_COLLECTION}" ]; then
