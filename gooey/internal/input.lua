3d7182b1b404f8908c02636d95438a3b7e8235b5
Jerakin <the_rensvind@hotmail.com>
2019-06-13 19:48:19 +0200

    missing parentheses

--- gooey/internal/input.lua
+++ gooey/internal/input.lua
@@ -106,7 +106,7 @@ function M.input(node_id, keyboard_type, action_id, action, config, refresh_fn)
 	input.node = node
 	input.refresh_fn = refresh_fn
 	
-	local use_marked_text = config and config.use_marked_text == nil and true or config.use_marked_text
+	local use_marked_text = (config and config.use_marked_text == nil) and true or config.use_marked_text
 	input.text = input.text or "" .. (not use_marked_text and input.marked_text or "")
 	input.marked_text = input.marked_text or ""
 	input.keyboard_type = keyboard_type
