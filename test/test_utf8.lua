1d6562a7b4f04d228ab0cd5404337f390bc66cf3
Björn Ritzl <bjorn.ritzl@gmail.com>
2019-07-05 08:06:53 +0200

    Added some utf8 tests

---
+++ test/test_utf8.lua
@@ -0,0 +1,36 @@
+local utf8 = require "gooey.internal.utf8"
+
+
+
+return function()
+	describe("utf8", function()
+		test("length", function()
+			assert(utf8.len("") == 0)
+			assert(utf8.len("abc") == 3)
+			assert(utf8.len("åäö") == 3)
+			assert(utf8.len("foo åäö bar") == 11)
+			assert(utf8.len("😄") == 1)	-- emoji smiley
+		end)
+
+		test("sub", function()
+			assert(utf8.sub("abc", 1) == "abc")
+			assert(utf8.sub("abc", 2) == "bc")
+			assert(utf8.sub("abc", 3) == "c")
+			assert(utf8.sub("abc", 1, 1) == "a")
+			assert(utf8.sub("abc", 2, 2) == "b")
+			assert(utf8.sub("abc", 3, 3) == "c")
+			assert(utf8.sub("abc", 1, 2) == "ab")
+			assert(utf8.sub("abc", 2, 3) == "bc")
+			assert(utf8.sub("åäö", 1) == "åäö")
+			assert(utf8.sub("åäö", 2) == "äö")
+			assert(utf8.sub("åäö", 3) == "ö")
+			assert(utf8.sub("åäö", 1, 1) == "å")
+			assert(utf8.sub("åäö", 2, 2) == "ä")
+			assert(utf8.sub("åäö", 3, 3) == "ö")
+			assert(utf8.sub("a😄b", 1, 1) == "a")
+			assert(utf8.sub("a😄b", 2, 2) == "😄")
+			assert(utf8.sub("a😄b", 1, -2) == "a😄")
+			assert(utf8.sub("a😄", 1, -2) == "a")
+		end)
+	end)
+end
